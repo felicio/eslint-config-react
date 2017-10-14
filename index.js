d4e64eb055ef85bab2f9d14cf1e4bc68ad332c7c
Felicio Mununga <felicio@users.noreply.github.com>
2017-10-14 19:00:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 'use strict'
 
 module.exports = {
-  extends: [
-    '@strv/javascript/coding-styles/recommended',
-    '@strv/javascript/environments/react/v15',
-    '@strv/javascript/environments/react/optional',
-    'prettier',
-  ],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 8,
   },
+  extends: [
+    '@strv/javascript/coding-styles/recommended',
+    '@strv/javascript/environments/react/v15',
+    '@strv/javascript/environments/react/optional',
+  ],
   rules: {
     'react/prefer-stateless-function': 'off',
     'template-tag-spacing': ['error', 'never'],
